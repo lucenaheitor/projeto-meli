bf88f6d8966d154f172027a1219f2f52a877d1bd
Heitor Goncalves Lucena Santos <you@example.com>
2025-01-12 14:41:26 -0300

    detail club corrigido

--- meli/src/main/java/heitor/projetofinal/meli/domain/club/dto/DetailClub.java
+++ meli/src/main/java/heitor/projetofinal/meli/domain/club/dto/DetailClub.java
@@ -1,14 +1,23 @@
 package heitor.projetofinal.meli.domain.club.dto;
 
-import heitor.projetofinal.meli.domain.club.Club;
 import heitor.projetofinal.meli.domain.state.State;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
-public record DetailClub(Long id, String name, State state, LocalDate date, boolean ativo) {
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class DetailClub {
 
-    public DetailClub(Club club){
-        this(club.getId(), club.getName(), club.getState(), club.getDate(), club.isAtivo());
-    }
+    private  Long id;
+    private String name;
+    private State state;
+    private LocalDate date;
+    private  boolean ativo;
+
+
 
 }
