e5f9a1389b29dd798241e5f3db64ba58c4ac11a9
Heitor Goncalves Lucena Santos <you@example.com>
2025-01-15 17:59:23 -0300

    WIP

--- meli/src/main/java/heitor/projetofinal/meli/domain/repository/MatchesRepository.java
+++ meli/src/main/java/heitor/projetofinal/meli/domain/repository/MatchesRepository.java
@@ -4,6 +4,6 @@ import heitor.projetofinal.meli.domain.match.Match;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
-public interface MatchRepository  extends JpaRepository<Match, Long> {
+public interface MatchesRepository  extends JpaRepository<Match, Long> {
 
 }
--- meli/src/main/java/heitor/projetofinal/meli/service/MatchService.java
+++ meli/src/main/java/heitor/projetofinal/meli/service/MatchService.java
@@ -2,7 +2,8 @@ package heitor.projetofinal.meli.service;
 
 import heitor.projetofinal.meli.domain.match.Match;
 import heitor.projetofinal.meli.domain.match.dto_match.CreateMatchDTO;
-import heitor.projetofinal.meli.domain.repository.MatchRepository;
+import heitor.projetofinal.meli.domain.repository.ClubRepository;
+import heitor.projetofinal.meli.domain.repository.MatchesRepository;
 import heitor.projetofinal.meli.infra.excepetion.ValidationExcepetion;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,16 +16,21 @@ public class MatchService {
     private ModelMapper modelMapper;
 
     @Autowired
-    private MatchRepository matchRepository;
+    private ClubRepository clubRepository;
+
+
 
     @Autowired
-    private  MatchRepository matchesRepository;
+    private MatchesRepository matchesRepository;
 
-//    public CreateMatchDTO createMatch(CreateMatchDTO createMatchDTO) {
-//
-//        if (matchesRepository.existsById(createMatchDTO.getId())) {
-//             throw new ValidationExcepetion(" club is not exists");
-//        }
-//
-//    }
+    public CreateMatchDTO createMatch(CreateMatchDTO dto) {
+
+
+        if (matchesRepository.existsById(dto.getId())) {
+             throw new ValidationExcepetion(" club is not exists");
+        }
+
+        if(clubRepository.existsById(dto.getId)
+
+    }
 }
