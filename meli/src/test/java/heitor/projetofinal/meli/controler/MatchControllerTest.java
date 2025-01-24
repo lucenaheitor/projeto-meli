f73445a6e4c9e1d97f6f6b580f1e743a3676ccce
Heitor Goncalves Lucena Santos <you@example.com>
2025-01-24 09:09:54 -0300

    test match update feito

--- meli/src/test/java/heitor/projetofinal/meli/controler/MatchControllerTest.java
+++ meli/src/test/java/heitor/projetofinal/meli/controler/MatchControllerTest.java
@@ -3,9 +3,11 @@ package heitor.projetofinal.meli.controler;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import heitor.projetofinal.meli.domain.club.club_dto.DetailClub;
 import heitor.projetofinal.meli.domain.club.club_dto.ListClubDTO;
+import heitor.projetofinal.meli.domain.club.club_dto.UpdateClubDTO;
 import heitor.projetofinal.meli.domain.match.dto_match.CreateMatchDTO;
 import heitor.projetofinal.meli.domain.match.dto_match.DetailMatchesDTO;
 import heitor.projetofinal.meli.domain.match.dto_match.ListMatches;
+import heitor.projetofinal.meli.domain.match.dto_match.UpdateMatchDTO;
 import heitor.projetofinal.meli.domain.repository.ClubRepository;
 import heitor.projetofinal.meli.domain.state.State;
 import heitor.projetofinal.meli.service.ClubService;
@@ -28,12 +30,11 @@ import java.util.Collections;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 
 @AutoConfigureMockMvc
 @SpringBootTest
-class MatchControllerTest {
+class  MatchControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
@@ -61,8 +62,8 @@ class MatchControllerTest {
         when(matchService.createMatch(any(CreateMatchDTO.class))).thenReturn(dto);
 
         var response = mockMvc.perform(post("/matches")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(dto)))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(dto)))
                 .andReturn().getResponse();
 
         Assertions.assertEquals(201, response.getStatus());
@@ -99,10 +100,23 @@ class MatchControllerTest {
     }
 
     @Test
-    void update() {
+    void update() throws Exception {
+        UpdateMatchDTO dto = new UpdateMatchDTO(
+                1L,
+                "morumbi teste",
+                LocalDate.of(2025, 02, 17)
+        );
+        when(matchService.updateMatch(any(UpdateMatchDTO.class))).thenReturn(dto);
+        var JsonContent = objectMapper.writeValueAsString(dto);
+
+        var response = mockMvc.perform(put("/matches")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(JsonContent))
+                .andReturn().getResponse();
+        Assertions.assertEquals(200, response.getStatus());
     }
 
-    @Test
-    void delete() {
-    }
+@Test
+void delete() {
+}
 }
